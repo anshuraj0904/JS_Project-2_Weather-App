7e44410bc112ed45e5592604c940c48081276769
anshuraj0904 <rajanshu0904@gmail.com>
2025-04-24 20:50:01 +0530

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# JS_Project-2_Weather-App
+# A simple and responsive weather app that fetches real-time weather information for a given city using the OpenWeatherMap API. The app displays current temperature, weather description, and feels-like temperature, providing an easy way to check the weather conditions at any location.
+
+# Stacks Used:
+1. HTML5: For building the structure of the app.
+
+2. CSS3: For styling the app and responsive design.
+
+3. JavaScript (Vanilla): For handling the app logic and fetching weather data from the OpenWeatherMap API.
+
+4. OpenWeatherMap API: Used to fetch weather data. 
--- script.js
+++ script.js
@@ -3,17 +3,18 @@ document.addEventListener('DOMContentLoaded', function(){
     const getWeatherInfoBtn = document.getElementById('get-weather-btn')
     const weatherInfo = document.getElementById('weather-info')
     const cityNameDisplay = document.getElementById('city-name')
-    const cityTempDisplay = document.getElementById('temperature')
     const cityDescDisplay = document.getElementById('description')
     const errMessage = document.getElementById('error-message')
+
     
-    const api_key = "Your-API-key-here" // Will get this into the environment variables later. 
+    // const api_key = "Your-API-key-here" // Will get this into the environment variables later. 
+
 
     getWeatherInfoBtn.addEventListener('click', async function(e){
         const cityName = cityInput.value.trim()
         
         if (cityName === "") return
-        console.log(cityName);
+        // console.log(cityName);
 
 
         // Two important things in API calls:-
@@ -36,10 +37,7 @@ document.addEventListener('DOMContentLoaded', function(){
         // Gets us the weather data, if that city exists
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`
 
-        const response = await fetch(url)
-
-        // console.log(typeof response);
-        // console.log(response.ok);
+        const response = await fetch(url)   
 
         if(!response.ok)
         {
@@ -50,20 +48,19 @@ document.addEventListener('DOMContentLoaded', function(){
         return data
     }
 
+      
     function displayWeatherData(data)
     {
         // will display the data
         const {name, main, weather}=data
         
-        // console.log(weather[0])
-        // console.log(main);
-
-
         cityNameDisplay.textContent = `City: ${name}`
         document.getElementById('current-temp-value').textContent = `Current Temp: ${main.temp}°C`
         document.getElementById('feels-like').textContent = `Feels Like: ${main.feels_like}°C`
-        cityDescDisplay.textContent = `Description: ${weather[0]['description']}`
-        
+        cityDescDisplay.textContent = `Description: ${weather[0]['description']}`  
+      
+
+                
         weatherInfo.classList.remove('hidden')
         errMessage.classList.add('hidden')
         cityInput.value = ""
